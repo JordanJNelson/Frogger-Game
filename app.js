c0c008234f201ffffd388febad264a4429edfbf1
JordanJNelson <litcircleapp@gmail.com>
2023-10-03 19:31:39 -0700

    Some Javascript

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+const timeLeftDisplay = document.querySelector('#time-left')
+const result = document.querySelector('#result')
+const startPauseButton = document.querySelector('#start-pause-button')
+const squares = document.querySelectorAll('.grid div')
+
+// Draw the frog
+function moveFrog() {
+  ctx.fillStyle = 'green';
+  ctx.fillRect(frog.x, frog.y, frog.width, frog.height);
+} 
+document.addEventListener('keyup', moveFrog)
+
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
@@ -10,12 +22,67 @@ const frog = {
   speed: 5,
 };
 
-// Draw the frog
-function drawFrog() {
-  ctx.fillStyle = 'green';
-  ctx.fillRect(frog.x, frog.y, frog.width, frog.height);
+function drawCars() {
+  ctx.fillStyle = 'red';
+  cars.forEach(car => {
+      ctx.fillRect(car.x, car.y, car.width, car.height);
+  });
 }
 
+function moveFrog(direction) {
+  if (direction === 'left' && frog.x > 0) {
+      frog.x -= frog.speed;
+  } else if (direction === 'right' && frog.x + frog.width < canvas.width) {
+      frog.x += frog.speed;
+  } else if (direction === 'up' && frog.y > 0) {
+      frog.y -= frog.speed;
+      score += 10; // Increase the score when the frog moves up
+  } else if (direction === 'down' && frog.y + frog.height < canvas.height) {
+      frog.y += frog.speed;
+  }
+}
+
+function updateGameArea() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawFrog();
+  drawCars();
+}
+cars.forEach(car => {
+  if (
+      frog.x < car.x + car.width &&
+      frog.x + frog.width > car.x &&
+      frog.y < car.y + car.height &&
+      frog.y + frog.height > car.y
+  ) {
+      // Collision detected, reset the frog
+      frog.x = canvas.width / 2 - 15;
+      frog.y = canvas.height - 30;
+      score = 0;
+  }
+});
+
+requestAnimationFrame(updateGameArea);
+
+
+// Handle keyboard input
+window.addEventListener('keydown', (event) => {
+switch (event.key) {
+  case 'ArrowUp':
+      moveFrog('up');
+      break;
+  case 'ArrowDown':
+      moveFrog('down');
+      break;
+  case 'ArrowLeft':
+      moveFrog('left');
+      break;
+  case 'ArrowRight':
+      moveFrog('right');
+      break;
+}
+});
+
+updateGameArea();
 // Update the game state
 function update() {
   // Handle user input here
@@ -27,7 +94,7 @@ function update() {
 
   // Redraw the canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawFrog();
+  moveFrog();
 
   // Request the next frame
   requestAnimationFrame(update);
