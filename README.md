0f75ea2476d1229b6a7c548f150fbba05a22a3d7
JordanJNelson <litcircleapp@gmail.com>
2023-10-04 21:20:10 -0700

    has moving logs and cars, starts and pauses, gameclock etc.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Frogger-Game
+This is a rendition of the classic frogger game. 
+
+This game is played by crossing without hitting any obstacles. Use the arrow keys to move forward, left, right or backward.
+
+You receive three lives and reset either when you successfully reach the other side, or when you are hit by an obstacle.
+
+This game was made using a mix of HTML, CSS and vanilla Javascript.
--- app.js
+++ app.js
@@ -2,13 +2,179 @@ const timeLeftDisplay = document.querySelector('#time-left')
 const result = document.querySelector('#result')
 const startPauseButton = document.querySelector('#start-pause-button')
 const squares = document.querySelectorAll('.grid div')
+const logsLeft = document.querySelectorAll('.log-left')
+const logsRight = document.querySelectorAll('.log-right')
+const carsLeft = document.querySelectorAll('.car-left')
+const carsRight = document.querySelectorAll('.car-right')
+
+console.log(squares)
+let currentIndex = 76;
+const width = 9
+let timerId
+let outcomeTimerId
+let currentTime = 20
 
 // Draw the frog
-function moveFrog() {
-  ctx.fillStyle = 'green';
-  ctx.fillRect(frog.x, frog.y, frog.width, frog.height);
-} 
-document.addEventListener('keyup', moveFrog)
+function moveFrog(e) {
+  squares[currentIndex].classList.remove('frog')
+
+  switch (e.key) {
+    case 'ArrowLeft':
+      if (currentIndex % width !== 0) currentIndex -= 1
+      break
+    case 'ArrowRight':
+      if (currentIndex % width < width - 1) currentIndex += 1
+      break
+    case 'ArrowUp':
+      if (currentIndex - width >= 0) currentIndex -= width
+      break
+    case 'ArrowDown':
+      if (currentIndex + width < width * width) currentIndex += width
+      break
+  }
+
+  squares[currentIndex].classList.add('frog')
+
+}
+// moving logs
+function autoMoveElements() {
+  currentTime--
+  timeLeftDisplay.textContent = currentTime
+  logsLeft.forEach(logLeft => moveLogLeft(logLeft))
+  logsRight.forEach(logRight => moveLogRight(logRight))
+  carsLeft.forEach(carLeft => moveCarLeft(carLeft))
+  carsRight.forEach(carRight => moveCarRight(carRight))
+}
+
+function checkOutcomes() {
+  lose()
+  win()
+}
+
+// move logs to the left
+function moveLogLeft(logLeft) {
+  switch (true) {
+    case logLeft.classList.contains('l1') :
+      logLeft.classList.remove('l1')
+      logLeft.classList.add('l2')
+      break 
+     case logLeft.classList.contains('l2') :
+        logLeft.classList.remove('l2')
+        logLeft.classList.add('l3')
+        break  
+     case logLeft.classList.contains('l3') :
+      logLeft.classList.remove('l3')
+      logLeft.classList.add('l4')
+      break 
+    case logLeft.classList.contains('l4') :
+      logLeft.classList.remove('l4')
+      logLeft.classList.add('l5')
+      break 
+    case logLeft.classList.contains('l5') :
+      logLeft.classList.remove('l5')
+      logLeft.classList.add('l1')
+      break 
+  }
+}
+
+// move logs to the right
+function moveLogRight(logRight) {
+  switch (true) {
+    case logRight .classList.contains('l1') :
+      logRight.classList.remove('l1')
+      logRight.classList.add('l5')
+      break 
+     case logRight.classList.contains('l2') :
+        logRight.classList.remove('l2')
+        logRight.classList.add('l1')
+        break  
+     case logRight.classList.contains('l3') :
+      logRight.classList.remove('l3')
+      logRight.classList.add('l2')
+      break 
+    case logRight.classList.contains('l4') :
+      logRight.classList.remove('l4')
+      logRight.classList.add('l3')
+      break 
+    case logRight.classList.contains('l5') :
+      logRight.classList.remove('l5')
+      logRight.classList.add('l4')
+      break 
+  }
+}
+
+
+function moveCarLeft(carLeft) {
+  switch (true) {
+    case carLeft.classList.contains('c1') :
+      carLeft.classList.remove('c1')
+      carLeft.classList.add('c2')
+      break 
+     case carLeft.classList.contains('c2') :
+        carLeft.classList.remove('c2')
+        carLeft.classList.add('c3')
+        break  
+     case carLeft.classList.contains('c3') :
+      carLeft.classList.remove('c3')
+      carLeft.classList.add('c1')
+      break
+  }
+}
+
+function moveCarRight(carRight) {
+  switch (true) {
+    case carRight.classList.contains('c1') :
+      carRight.classList.remove('c1')
+      carRight.classList.add('c3')
+      break 
+     case carRight.classList.contains('c2') :
+        carRight.classList.remove('c2')
+        carRight.classList.add('c1')
+        break  
+     case carRight.classList.contains('c3') :
+      carRight.classList.remove('c3')
+      carRight.classList.add('c2')
+      break
+  }
+}
+
+function lose() {
+  if (
+    squares[currentIndex].classList.contains('c1') || 
+    squares[currentIndex].classList.contains('l4') ||
+    squares[currentIndex].classList.contains('l5') ||
+    currentTime <= 0
+    ) {
+    resultDisplay.textContent = 'You lose!';
+    clearInterval(timerId)
+    clearInterval(outcomeTimerId)
+    squares[currentIndex].classList.remove('frog')
+    document.removeEventListener('keyup', moveFrog)
+  }
+}
+
+function win() {
+  if (squares[currentIndex].classList.contains('ending-block')) {
+    resultDisplay.textContent = 'You win!'
+    clearInterval(timerId)
+    clearInterval(outcomeTimerId)
+    document.removeEventListener('keyup', moveFrog)
+  }
+}
+
+startPauseButton.addEventListener('click', () => {
+   if(timerId){
+      clearInterval(timerId)
+      clearInterval(outcomeTimerId)
+      outcomeTimerId = null
+      timerId = null
+      document.removeEventListener('keyup', moveFrog)
+   } else {
+    timerId = setInterval(autoMoveElements, 1000)
+    outcomeTimerId = setInterval(checkOutComes, 50)
+    document.addEventListener('keyup', moveFrog)
+  }
+})
 
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
@@ -25,20 +191,20 @@ const frog = {
 function drawCars() {
   ctx.fillStyle = 'red';
   cars.forEach(car => {
-      ctx.fillRect(car.x, car.y, car.width, car.height);
+    ctx.fillRect(car.x, car.y, car.width, car.height);
   });
 }
 
 function moveFrog(direction) {
   if (direction === 'left' && frog.x > 0) {
-      frog.x -= frog.speed;
+    frog.x -= frog.speed;
   } else if (direction === 'right' && frog.x + frog.width < canvas.width) {
-      frog.x += frog.speed;
+    frog.x += frog.speed;
   } else if (direction === 'up' && frog.y > 0) {
-      frog.y -= frog.speed;
-      score += 10; // Increase the score when the frog moves up
+    frog.y -= frog.speed;
+    score += 10; // Increase the score when the frog moves up
   } else if (direction === 'down' && frog.y + frog.height < canvas.height) {
-      frog.y += frog.speed;
+    frog.y += frog.speed;
   }
 }
 
@@ -49,15 +215,15 @@ function updateGameArea() {
 }
 cars.forEach(car => {
   if (
-      frog.x < car.x + car.width &&
-      frog.x + frog.width > car.x &&
-      frog.y < car.y + car.height &&
-      frog.y + frog.height > car.y
+    frog.x < car.x + car.width &&
+    frog.x + frog.width > car.x &&
+    frog.y < car.y + car.height &&
+    frog.y + frog.height > car.y
   ) {
-      // Collision detected, reset the frog
-      frog.x = canvas.width / 2 - 15;
-      frog.y = canvas.height - 30;
-      score = 0;
+    // Collision detected, reset the frog
+    frog.x = canvas.width / 2 - 15;
+    frog.y = canvas.height - 30;
+    score = 0;
   }
 });
 
@@ -66,20 +232,20 @@ requestAnimationFrame(updateGameArea);
 
 // Handle keyboard input
 window.addEventListener('keydown', (event) => {
-switch (event.key) {
-  case 'ArrowUp':
+  switch (event.key) {
+    case 'ArrowUp':
       moveFrog('up');
       break;
-  case 'ArrowDown':
+    case 'ArrowDown':
       moveFrog('down');
       break;
-  case 'ArrowLeft':
+    case 'ArrowLeft':
       moveFrog('left');
       break;
-  case 'ArrowRight':
+    case 'ArrowRight':
       moveFrog('right');
       break;
-}
+  }
 });
 
 updateGameArea();
