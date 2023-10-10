5396531652bbcfa2ccf15ba2ff3b35b331ec08fc
JordanJNelson <litcircleapp@gmail.com>
2023-10-09 17:08:10 -0700

    took out the 0 from result

--- app.js
+++ app.js
@@ -306,9 +306,18 @@ function initObstacles(){
   for(let i = 0; i < 2; i++){
     let x = i * 350;
     carsArray.push(new Obstacle(x, canvas.height - grid * 2 - 20, grid * 2, grid, 1, 'car'));
-  }
+  };
 }
-
+// lane 2
+for (let i = 0; i < 2; i++){
+  let x = i * 300;
+  carsArray.push(new Obstacle(x, canvas.height - grid * 3 - 20, grid * 2, grid, -2, 'car'))
+}
+// lane 3
+for (let i =0; i < 2; i++){
+  let x = i * 400;
+  carsArray.push(new Obstacle(x, canvas.height - grid * 4 - 20, grid * 2, grid, 2, 'car'))
+}
 
 initObstacles();
 
